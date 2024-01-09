7c8c15ec8311b2afafa29c1a42ab118ff37e2ac7
fh-rabbi <frabby983@gmail.com>
2024-01-09 14:43:27 +0600

    Updated note card dark mode hover style

--- src/components/ui/NoteCard.jsx
+++ src/components/ui/NoteCard.jsx
@@ -5,7 +5,7 @@ import { BookText } from "lucide-react";
 
 export const NoteCard = ({ note }) => {
   return (
-    <li className="flex rounded hover:bg-gray-100">
+    <li className="flex rounded hover:bg-gray-100 dark:hover:bg-gray-700">
       <Link to={`/view-note/${note?.$id}`} className="note_list">
         <span className="text-dark-5 dark:text-off-white">{note?.icon ? note.icon : <BookText size={20} />}</span>
         <h2 className="text-dark-5 dark:text-off-white">{note?.title.slice(0, 30)} {note?.title.length > 30 && "..."}</h2>
